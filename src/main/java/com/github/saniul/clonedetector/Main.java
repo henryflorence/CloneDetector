3a8537389a790b9ab8dd9f6d8ad03792a7375986
Saniul Ahmed <saniul.ahmed@gmail.com>
2011-03-21 17:56:07 +0000

    Main calls the FileProcessors and the Algorithm

--- src/main/java/com/github/saniul/clonedetector/Main.java
+++ src/main/java/com/github/saniul/clonedetector/Main.java
@@ -1,34 +1,45 @@
 package com.github.saniul.clonedetector;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import com.github.saniul.clonedetector.preprocessor.Normalizer;
-import com.github.saniul.clonedetector.preprocessor.Remover;
-
+import com.github.saniul.clonedetector.preprocessor.EmptyLineRemover;
 
 /**
  * Main class that strings together the preprocessor and main algorithm
- *
+ * 
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-    	Remover remover = new Remover("./testFiles/Test.java");
-    	remover.buildLineMap();
-    	remover.removeEmptyLines();
-    	System.out.println();
-    	Normalizer normalizer = new Normalizer();
-    	normalizer.normalizeFile("./testFiles/Test.java");
-    	
-//		try {
-//			List<MainAlgorithm.CloneLines> lines = new MainAlgorithm().check(args[0]);
-//			
-//			for(MainAlgorithm.CloneLines line : lines)
-//	    		System.out.println(line);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-    }
+public class Main {
+	public static void main(String[] args) {
+		File original = new File("./testFiles/Test.java");
+		EmptyLineRemover emptyLineRemover = new EmptyLineRemover(original);
+		Normalizer normalizer = new Normalizer(emptyLineRemover.process());
+		File processed = normalizer.process();
+		
+		MainAlgorithm algo = new MainAlgorithm();
+		List<CloneLines> cloneLines = null;
+		try {
+			cloneLines = algo.check(processed);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		cloneLines = normalizer.remap(cloneLines);
+		cloneLines = emptyLineRemover.remap(cloneLines);
+		for(CloneLines cl:cloneLines){
+			System.out.println(cl);
+		}
+		// try {
+		// List<MainAlgorithm.CloneLines> lines = new
+		// MainAlgorithm().check(args[0]);
+		//
+		// for(MainAlgorithm.CloneLines line : lines)
+		// System.out.println(line);
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+	}
 }
