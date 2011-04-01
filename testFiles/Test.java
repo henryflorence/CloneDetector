ae899591a5edc5ecdd4c81ec5e51bd8a2d4764db
Min Park <dyss.min@gmail.com>
2011-04-01 14:17:16 +0100

    local changes

--- testFiles/Test.java
+++ testFiles/Test.java
@@ -7,9 +7,10 @@ class Test {
         }
     }
     public void method2() {
-        int n = 0;
-        double z = 1.0f;        
-        for(n = 0; n < 10; n++) {
+        int n,z = 0;
+
+        for(n = 0; n < 10; n++) 
+		{
             z += 1.0f;
         }
     }
